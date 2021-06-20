92a47eabb0a60ee80edfae6ca23ee96bdfd90f3f
hahacandy <58638224+hahacandy@users.noreply.github.com>
2021-06-20 10:29:18 +0900

    Update anime_downloader.py

--- anime_downloader.py
+++ anime_downloader.py
@@ -152,7 +152,7 @@ while True:
                     if len(ep_number) < 2:
                         ep_number = '0' + ep_number
                     print(anime_name, "ep"+str(idx+1) + " downloading", str(idx+1) + "/" + str(len(ani_ep_list)))
-                    cmd = "aria2c -c -x 4 -d "+save_dir+" -m 5 -o " + save_anime_name + "_ep" + ep_number + ".mp4 " + ''.join(ani_ep['mp4'])
+                    cmd = "aria2c -c -x 4 -d "+save_dir+" -m 5 -o " + save_anime_name + " " + ep_number + ".mp4 " + ''.join(ani_ep['mp4'])
                     result = os.system(cmd)
                     if result == 0:
                         print(anime_name, "ep"+str(idx+1) + " downloaded", str(idx+1) + "/" + str(len(ani_ep_list)))
