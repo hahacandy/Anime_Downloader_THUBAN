3642acb52caf0aeb82a5267b16eda4cb70700b1a
hahacandy <58638224+hahacandy@users.noreply.github.com>
2021-10-27 02:29:46 +0900

    애니메 받는 사이트 변경함

--- anime_downloader.py
+++ anime_downloader.py
@@ -5,9 +5,11 @@ import re
 import os
 import time
 import shutil
+import threading
+from selenium.webdriver.common.keys import Keys
 
 save_dir = "Y:\Anime"
-URL = 'https://animebee.to'
+URL = 'https://123animes.mobi'
 anime_name = ''
 
 def driver_set():
@@ -37,8 +39,7 @@ def createFolder(directory):
             os.makedirs(directory)
     except OSError:
         print ('Error: Creating directory. ' +  directory)
-        
-        
+
 def search_anime():
     global URL, anime_name
     
@@ -52,15 +53,19 @@ def search_anime():
         if response.status_code == 200:
             html = response.text
             soup = BeautifulSoup(html, 'html.parser')
-            titles = soup.select(".flw-item")
+            titles = soup.select(".item > .inner")
 
-            if len(titles) > 0:
+            if "No Results Found" not in titles.__str__():
 
                 for title in titles:
-                    searched_anime_lists.append({'href' : title.select(".film-name > .dynamic-name")[0]['href'], 'name' : title.select(".film-name > .dynamic-name")[0].text})
+                    searched_anime_lists.append({'href' : title.select(".name")[0]['href'], 'name' : title.select(".name")[0].text})
+
+
+
 
                 for idx, searched_anime_list in enumerate(searched_anime_lists):
                     print(idx+1, " : ",searched_anime_list['name'])
+
 
                 ani_sel = 0
                 while True:
@@ -80,49 +85,39 @@ def search_anime():
                 print("Your choice : " + anime_name)
                 print()
 
-                response = requests.get(searched_anime_lists[ani_sel-1]['href']) 
-                html = response.text
-                soup = BeautifulSoup(html, 'html.parser')
-                ani_page = soup.select(".film-buttons > a")[0]['href']
-
-
+                ani_page = URL+searched_anime_lists[ani_sel-1]['href']
                 return ani_page
 
-            else:
-                print("No Results Found!")
-
-        else : 
-            print(response.status_code)
-
-
-def get_ani_ep_pages_and_urls(_ani_page):
+def get_anime_pages(driver, ani_page):
     print("anime page loding...")
 
     ## Chrome의 경우 | 아까 받은 chromedriver의 위치를 지정해준다.
-    driver = driver_set()
-    driver.implicitly_wait(3)
-    driver.get(_ani_page)
+    driver.get(ani_page)
 
     eps_els = None
     while True:
         try:
-            eps_els = driver.find_element_by_id("episodes-page-1").find_elements_by_tag_name('a')
+            eps_els = driver.find_elements_by_class_name("episodes, range")[0].find_elements_by_tag_name('li')
             break
         except:
             print("anime page load fail, retry")
 
     ani_ep_list = []
     for idx, ep_el in enumerate(eps_els):
-        page = ep_el.get_attribute('href')
+        page = ep_el.find_element_by_tag_name('a').get_attribute('href')
         ani_ep_list.append({'page': page})
         print("anime page loding " + str(idx+1) + "/" + str(len(eps_els)))
 
     print("anime page load clear")
     print()
-    
+    return ani_ep_list
+
+def get_anime_down_url(driver, ani_ep_list):
     print("anime info loding...")
     for idx, ani_ep in enumerate(ani_ep_list):
 
+                
+        #가끔 페이지 안뜰떄가 잇음 그럴땐 새로고침
         i = 0
         while i >= 0:
             i = 0
@@ -131,98 +126,106 @@ def get_ani_ep_pages_and_urls(_ani_page):
             while i < 5:
                 i = i + 1
                 try:
-                    driver.find_elements_by_tag_name('iframe')[0]
+                    driver.find_element_by_xpath('//*[@id="player"]/iframe')
                     i = -1
                     break
                 except:
                     time.sleep(1)
-                    
+
+        
+        #blob로 된 다운로드 주소는 어차피 사용불가이기때문에 m3u8을 찾는다
         i = 0
         while i >= 0:
             try:
                 driver.switch_to.frame(driver.find_elements_by_tag_name('iframe')[0])
+                driver.find_element_by_tag_name('video').send_keys(Keys.ENTER)
                 mp4 = driver.find_element_by_tag_name('video').get_attribute('src')
 
                 if 'blob' in mp4:
                     JS_get_network_requests = "var performance = window.performance || window.msPerformance || window.webkitPerformance || {}; var network = performance.getEntries() || {}; return network;"
                     network_requests = driver.execute_script(JS_get_network_requests)
                     for n in network_requests:
-                        if ".m3u8" in n["name"] and "list" not in n["name"]: 
-                            mp4 = n["name"]
-
-                            print("주소:" + mp4)
-
-                            ani_ep['mp4'] = mp4
-                            i = -1 #while break
-                            break
+                        if "list" not in n["name"]:
+                            if ".m3u8" in n["name"]: 
+                                mp4 = n["name"]
+                                ani_ep['mp4'] = mp4
+                                i = -1 #while break
+                                break
                 elif '.mp4' in mp4:
                     break
-                    
+                
+ 
             except:
+                #에러났으면 초점을 iframe 에서 다시 원래대로 복원해야함
                 driver.switch_to.default_content()   
+                
+            #재생버튼 누르면 가끔 광고탭으로 이동함 그래서 1번쨰 탭으로 이동시킴
+            driver.switch_to.window(driver.window_handles[0]) 
 
-
+                
+        print("주소:" + mp4)
+        driver.switch_to.default_content()   
         print("anime info loding " + str(idx+1) + "/" + str(len(ani_ep_list)))
 
-    driver.quit()
+
     print("anime info load clear")
-    print() 
-    
-    return ani_ep_list
-
-
-
+    print()
 
 def download_anime(_ani_ep_list):
     global anime_name, save_dir
     
-    file = './m3u8_To_Mp4.mp4'
-
-    if os.path.isfile(file):
-        os.remove(file)
-    
     print('anime downloading...')
     
-    save_anime_name = anime_name.replace(' ', '_').replace(':','')
+    save_anime_name = anime_name.replace(' ', '_').replace(':','').replace('?', '')
     save_dir2 = save_dir + "\\" + save_anime_name
-    createFolder("ani")
     createFolder(save_dir2)
     
+    thread_list = []
     for idx, ani_ep in enumerate(_ani_ep_list):
-        result = 1
-        down_try = 0
-        while result == 1 and down_try < 5:
-            down_try = down_try + 1
-
-            ep_number = str(idx+1)
-            if len(ep_number) < 2:
-                ep_number = '0' + ep_number
-            print(anime_name, "ep"+str(idx+1) + " downloading", str(idx+1) + "/" + str(len(_ani_ep_list)))
-            if ".m3u8" in ani_ep['mp4']:
-                ani_save_path = save_dir2 + "/" + save_anime_name + "_ep" + ep_number + ".mp4"
-                
-                os.system('ffmpeg -i ' + ani_ep['mp4'] + ' -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 ' + ani_save_path)
-
-                if os.path.isfile(ani_save_path):
-                    result = 0
-            else:
-                cmd = "aria2c -c -x 4 -d " + save_dir2+" -m 5 -o " + save_anime_name + "_ep" + ep_number + ".mp4 " + ''.join(ani_ep['mp4'])
-                print(cmd)
-                result = os.system(cmd)
-            if result == 0:
-                print(anime_name, "ep"+str(idx+1) + " downloaded", str(idx+1) + "/" + str(len(_ani_ep_list)))
-                print()
-            else:
-                print("Retry" ,anime_name, "ep"+str(idx+1) + " downloading", str(idx+1) + "/" + str(len(_ani_ep_list)))
+        t = threading.Thread(target=download_anime2, args=(idx, ani_ep, _ani_ep_list, save_anime_name, save_dir2, thread_list))
+        t.daemon = True 
+        t.start()
+        thread_list.append(t)      
+ 
+    for t in thread_list:
+        t.join()
 
     print(anime_name + " all ani download complete")
     
     
-    
-while True:
+def download_anime2(idx, ani_ep, _ani_ep_list, save_anime_name, save_dir2, thread_list):
+    global anime_name, save_dir
 
-    ani_page = search_anime()
+    result = 1
+    down_try = 0
+    while result == 1 and down_try < 5:
+        down_try = down_try + 1
 
-    ani_ep_list = get_ani_ep_pages_and_urls(ani_page)
+        ep_number = str(idx+1)
+        if len(ep_number) < 2:
+            ep_number = '0' + ep_number
+        print(anime_name, "ep"+str(idx+1) + " downloading", str(idx+1) + "/" + str(len(_ani_ep_list)))
+        if ".m3u8" in ani_ep['mp4']:
+            ani_save_path = save_dir2 + "/" + save_anime_name + "_ep" + ep_number + ".mp4"
+            
+            os.system('ffmpeg -i ' + ani_ep['mp4'] + ' -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 ' + ani_save_path)
 
-    download_anime(ani_ep_list)
+            if os.path.isfile(ani_save_path):
+                result = 0
+        else:
+            cmd = "aria2c -c -x 4 -d " + save_dir2+" -m 5 -o " + save_anime_name + "_ep" + ep_number + ".mp4 " + ''.join(ani_ep['mp4'])
+            print(cmd)
+            result = os.system(cmd)
+        if result == 0:
+            print(anime_name, "ep"+str(idx+1) + " downloaded", str(idx+1) + "/" + str(len(_ani_ep_list)))
+            print()
+        else:
+            print("Retry" ,anime_name, "ep"+str(idx+1) + " downloading", str(idx+1) + "/" + str(len(_ani_ep_list)))
+
+ani_page = search_anime()
+driver = driver_set()
+driver.implicitly_wait(3)
+ani_ep_list = get_anime_pages(driver, ani_page)
+get_anime_down_url(driver, ani_ep_list)
+driver.quit()
+download_anime(ani_ep_list)
