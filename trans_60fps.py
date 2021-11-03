450f6d4b2b9a39d7ce02282617a3e7da2f70ce9d
hahacandy <58638224+hahacandy@users.noreply.github.com>
2021-11-04 00:19:53 +0900

    Update trans_60fps.py

--- trans_60fps.py
+++ trans_60fps.py
@@ -21,37 +21,35 @@ def trans_to_60fps(cmd):
     os.system(cmd)
     print(cmd)
 
-while True:
 
-    for path, dirs, files in os.walk(anime_path):
-        for file in files:
-            file_path = os.path.join(path, file)
-            path2 = pathlib.Path(file_path)
+for path, dirs, files in os.walk(anime_path):
+    for file in files:
+        file_path = os.path.join(path, file)
+        path2 = pathlib.Path(file_path)
 
-            change_name = str(path2.parent) + '/60FPS_' + file
-            
-            if os.path.isfile(change_name) == False and '60FPS' not in path2.name:
-            
-                if path2.suffix == '.mp4':
-                    cmd = ffmpeg_path + 'ffmpeg.exe -i \"' + file_path +\
-                    '\"  -filter:v  \"minterpolate=\'mi_mode=mci:mc_mode=aobmc:vsbmc=1:fps=60:me=ds\'\" ' + change_name
-                    t = threading.Thread(target=trans_to_60fps, args=(cmd,))
-                    t.daemon = True 
-                    t.start()
-                    thread_list.append(t)
-                    
-                elif path2.suffix == '.srt':
-                    shutil.copyfile(str(file_path), change_name)
-                    print('cp:' + str(file_path) + '->' + change_name)
-                    
-                    
-                #동시에 최대 4개까지만 인코딩 가능하게.
-                if len(thread_list) >= 4:
-                    for t in thread_list:
-                        t.join()
-                    #쓰레드 배열 삭제
-                    thread_list.clear()
-
-                print()
+        change_name = str(path2.parent) + '/60FPS_' + file
+        
+        if os.path.isfile(change_name) == False and '60FPS' not in path2.name:
+        
+            if path2.suffix == '.mp4':
+                cmd = ffmpeg_path + 'ffmpeg.exe -i \"' + file_path +\
+                '\"  -filter:v  \"minterpolate=\'mi_mode=mci:mc_mode=aobmc:vsbmc=1:fps=60:me=ds\'\" ' + change_name
+                t = threading.Thread(target=trans_to_60fps, args=(cmd,))
+                t.daemon = True 
+                t.start()
+                thread_list.append(t)
                 
-    time.sleep(60)
+            elif path2.suffix == '.srt':
+                shutil.copyfile(str(file_path), change_name)
+                print('cp:' + str(file_path) + '->' + change_name)
+                
+                
+            #동시에 최대 n개까지만 인코딩 가능하게.
+            if len(thread_list) >= 5:
+                for t in thread_list:
+                    t.join()
+                #쓰레드 배열 삭제
+                thread_list.clear()
+
+            print()
+            
