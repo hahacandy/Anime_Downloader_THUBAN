e44573c77b00de2f7c4114dcfcbdf57e91f5e3f5
hahacandy <58638224+hahacandy@users.noreply.github.com>
2021-12-03 22:14:29 +0900

    Update anime_downloader.py

--- anime_downloader.py
+++ anime_downloader.py
@@ -159,6 +159,8 @@ def get_anime_down_url(driver, ani_ep_list):
                             ani_ep['referer'] = referer
                             print("레퍼런스:" + ani_ep['referer'])
                             break
+                    if 'referer' in ani_ep['referer']:
+                        break
                 elif '.mp4' in mp4:
                     ani_ep['mp4'] = mp4
                     break
